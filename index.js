10f8a3a2c9ec233f0b49405aad718a013e2245e2
ShrekBenson <daardibujos@gmail.com>
2024-02-23 22:28:08 -0600

    Add connection to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require ('apollo-server');
 const typeDefs = require ('./db/schema');
 const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
+
+conectarDB();
 
 const server = new ApolloServer({typeDefs, resolvers});
 server.listen().then(({url}) => {
